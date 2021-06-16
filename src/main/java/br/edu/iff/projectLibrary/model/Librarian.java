679acd16b95e7660090381c0a74cd5266bcfc79a
Rhavgar <53013070+Rhavgar@users.noreply.github.com>
2021-06-16 18:36:11 -0300

    CREATED : Librarian CLASS

---
+++ src/main/java/br/edu/iff/projectLibrary/model/Librarian.java
@@ -0,0 +1,18 @@
+package br.edu.iff.projectLibrary.model;
+
+
+public class Librarian extends Person
+{
+    private String password;
+
+    public String getPassword() 
+    {
+        return password;
+    }
+
+    public void setPassword(String password) 
+    {
+        this.password = password;
+    }
+    
+}
