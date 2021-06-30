830ac318845fb93cf8c8bc302bc354d6ccf64259
Rhavgar <53013070+Rhavgar@users.noreply.github.com>
2021-06-30 20:06:06 -0300

    FIXED: Removed Inheritance

--- src/main/java/br/edu/iff/projectLibrary/model/Alert.java
+++ src/main/java/br/edu/iff/projectLibrary/model/Alert.java
@@ -9,8 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
@@ -21,7 +19,6 @@ import javax.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
 public class Alert implements Serializable
 {
     private static final long serialVersionUID = 1L;
