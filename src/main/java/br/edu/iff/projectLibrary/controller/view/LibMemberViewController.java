d582f102644669a342d5756264be0b468014da63
Rhavgar <53013070+Rhavgar@users.noreply.github.com>
2021-07-21 18:53:11 -0300

    INSERTED : Password field and actions.

--- src/main/java/br/edu/iff/projectLibrary/controller/view/LibMemberViewController.java
+++ src/main/java/br/edu/iff/projectLibrary/controller/view/LibMemberViewController.java
@@ -2,17 +2,21 @@ package br.edu.iff.projectLibrary.controller.view;
 
 import br.edu.iff.projectLibrary.model.LibMember;
 import br.edu.iff.projectLibrary.service.LibMemberService;
+import java.util.ArrayList;
+import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(path = "/libmembers")
@@ -36,11 +40,17 @@ public class LibMemberViewController
     }
     
     @PostMapping(path = "/libmember")
-    public String save(@Valid @ModelAttribute LibMember libmember, BindingResult result, Model model)
+    public String save(@Valid @ModelAttribute LibMember libmember, BindingResult result, @RequestParam("confirmPassword") String confirmPassword, Model model)
     {
         if(result.hasErrors())
         {
             model.addAttribute("errorMsg", result.getAllErrors());
+            return "formLibMember";
+        }
+        
+        if(!libmember.getPassword().equals(confirmPassword))
+        {
+            model.addAttribute("errorMsg", new ObjectError("libmember", "Campo da Senha e Confirmar Senha devem ser iguais."));
             return "formLibMember";
         }
         
@@ -70,9 +80,17 @@ public class LibMemberViewController
     @PostMapping(path = "/libmember/{id}")
     public String update(@Valid @ModelAttribute LibMember libmember, BindingResult result,@PathVariable("id") Long id, Model model)
     {
-        if(result.hasErrors())
+        List<FieldError> list = new ArrayList<>();
+        for(FieldError fe : result.getFieldErrors())
         {
-            model.addAttribute("errorMsg", result.getAllErrors());
+            if(!fe.getField().equals("password"))
+            {
+                list.add(fe);
+            }
+        }
+        if(!list.isEmpty())
+        {
+            model.addAttribute("errorMsg", list);
             return "formLibMember";
         }
         
@@ -80,7 +98,7 @@ public class LibMemberViewController
         
         try
         {
-            service.update(libmember, null, null, null);
+            service.update(libmember, "", "", "");
             model.addAttribute("successMsg", "Membro atualizado com sucesso.");
             model.addAttribute("libmember", libmember);
             return "formLibMember";
